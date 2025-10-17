06b59d7d25db1f3eb05836ee35107a9b651c748b
YH949494 <teohyaohui@gmail.com>
2025-10-17 09:24:56 +0800

    Update vouchers.py

--- vouchers.py
+++ vouchers.py
@@ -47,7 +47,7 @@ def verify_telegram_init_data(init_data_raw):
         data = dict(urllib.parse.parse_qsl(init_data_raw, keep_blank_values=True))
         check_hash = data.pop("hash", None)
         data_check_string = "\n".join([f"{k}={v}" for k, v in sorted(data.items())])
-        secret_key = hashlib.sha256(bot_token.encode()).digest()
+        secret_key = hashlib.sha256(BOT_TOKEN.encode()).digest()
         h = hmac.new(secret_key, data_check_string.encode(), hashlib.sha256).hexdigest()
         return h == check_hash, data
     except Exception as e:
