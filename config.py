66c5cd9cfd0c9de8bd524c8634fbe7a01b3b7f88
YH949494 <teohyaohui@gmail.com>
2025-11-17 18:44:43 +0800

    Update config.py

--- config.py
+++ config.py
@@ -5,6 +5,7 @@ KL_TZ = ZoneInfo("Asia/Kuala_Lumpur")
 
 # Check-in XP settings (single source of truth)
 XP_BASE_PER_CHECKIN = 20
+FIRST_CHECKIN_BONUS = 200
 STREAK_MILESTONES = {
     7: 50,
     14: 150,
